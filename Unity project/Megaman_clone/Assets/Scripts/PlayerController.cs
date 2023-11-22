2bd03b7cb2fd2504a99e5519348e7a0629721f0f
Einari <makirantaeinari@gmail.com>
2023-11-22 12:20:27 +0200

    PlayerController base READY!!

--- Unity project/Megaman_clone/Assets/Scripts/PlayerController.cs
+++ Unity project/Megaman_clone/Assets/Scripts/PlayerController.cs
@@ -1,229 +1,113 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class PlayerController : MonoBehaviour {
-
-    public float maxHorizontalOffset;
-    public float maxVerticalOffset;
-    public float verticalOffset;
-    float currentVerticalOffset;
-    public float fallAccelerationDuration;
-    float verticalAccelerator = 0;
-    float jumpOffset;
-
-    bool goingRight = true;
-
-    //groundCheck
-    public LayerMask solids;
-    public Vector2 gcheckDimensions;
-    public float gcheckDistance;
-
-    bool hasLongJumped = false;
-    bool hasReleasedJumpDuringJump = true;
-
-    //jump times
-    public float shortJumpTime;
-    public float longJumpTimeAddition;
-    float jumpTime;
-    float currentJumpDuration;
-
-    //jump key press times
-    float jumpKeyPressTimer;
-    public float longJumpTriggerTime;
-   
-    //movement directions/speeds
-    float moveDirection;
-    public int verticalDirection = -1;
-    float xOffset;
-    public float yOffset;
-
-    bool jumpingUp;
-    public bool grounded;
-    public bool offSetting = false;
-
+public class PlayerController : MonoBehaviour
+{
     Rigidbody2D rb;
+
+    public float maxHorizontalVelocity;
+    public float minHorziontalVelocityMultiplier;
+    public float jumpVelocity;
+    public float moveDirection;
+    public float defaultGravityScale;
+
+    public float jumpWindow;
+    public float horizontalAccelerationTime;
+    public float initialHorizontalOffset;
+    bool changingHorizontalDirection;
+
+    public Vector2 gcheckDimensions;
+
+    float jumpKeyPressTimer;
+    public float maxJumpTime;
+    public float minJumpTime;
+
+    bool jumpingUp = false;
+    bool groundCheckAllowed = true;
+    bool grounded;
+    public LayerMask solids;
 
     void Awake() {
         rb = GetComponent<Rigidbody2D>();
+        groundCheckAllowed = true;
     }
 
-    Vector3 GetNewPosition(Vector3 previousPosition)
-    {
-        moveDirection = Input.GetAxisRaw("Horizontal");
-        if (moveDirection == 1)
-            goingRight = true;
-        else if (moveDirection == -1)
-            goingRight = false;
-        xOffset = moveDirection * maxHorizontalOffset * Time.deltaTime;
-        if (!grounded || jumpingUp) { 
-            yOffset = verticalOffset * Time.deltaTime;
-            print("falling");
+    void Update() {
+
+        if (Input.GetAxisRaw("Horizontal") != 0 && !changingHorizontalDirection) {
+            StartCoroutine(HorizontalOffsetChange());
+            changingHorizontalDirection = true;
         }
-        else { 
-            yOffset = 0;
-            offSetting = false;
+
+        if (Input.GetAxisRaw("Jump") != 0 && !jumpingUp) {
+            StartCoroutine(Jump());
+            print("jumpingUp");
+            jumpingUp = true;
         }
-        return new Vector3(xOffset + previousPosition.x, yOffset + previousPosition.y, previousPosition.z);
+
+        if (grounded && Input.GetAxisRaw("Jump") == 0) 
+            jumpingUp = false;
     }
 
-    IEnumerator Jump()
-    {
-        hasReleasedJumpDuringJump = false;
-        hasLongJumped = false;
-        grounded = false;
-        jumpingUp = true;
-        verticalDirection = 1;
-        jumpTime += shortJumpTime;
-        while (jumpTime > 0)
-        {
-            //Debug.Log(jPressTimer);
-            jumpTime -= Time.deltaTime;
-            verticalAccelerator += Time.deltaTime;
+    void FixedUpdate() {
+        rb.velocity = new Vector2(maxHorizontalVelocity * moveDirection, rb.velocity.y);
+        if (!groundCheckAllowed) {
+            grounded = false;
+            return;
+        }
+        grounded = null != Physics2D.OverlapBox(transform.position, gcheckDimensions,
+                                                    0, solids);
+    }
+
+    IEnumerator HorizontalOffsetChange() {
+        float horizontalAccelerationTimer = 0;
+        moveDirection = Input.GetAxisRaw("Horizontal") * minHorziontalVelocityMultiplier;
+        rb.position = new Vector2(rb.position.x+initialHorizontalOffset*moveDirection/minHorziontalVelocityMultiplier
+                                  , rb.position.y);
+        while (horizontalAccelerationTimer < horizontalAccelerationTime && Input.GetAxisRaw("Horizontal")
+               == moveDirection / minHorziontalVelocityMultiplier) {
+            horizontalAccelerationTimer += Time.deltaTime;
             yield return null;
         }
-        verticalDirection = -1;
-        jumpingUp = false;
+        moveDirection /= minHorziontalVelocityMultiplier;
+        while (Input.GetAxisRaw("Horizontal") == moveDirection) {
+            yield return null;
+        }
+        moveDirection = 0;
+        changingHorizontalDirection = false;
     }
 
-    IEnumerator ChangeVerticalOffset(int currentDirection) {
-        
-        offSetting = true;
-        verticalAccelerator = 0.01f;
-        while (verticalDirection == currentDirection) {
-
-        yield return null;
-        }
-        verticalAccelerator = 0.01f;
-        offSetting = false;
-    }
-
-    void Update()
-    {
-        float playerVerticalInput = Input.GetAxisRaw("Jump");
-
-        if (playerVerticalInput != 0 && !jumpingUp && grounded && hasReleasedJumpDuringJump)
-        {
-            StartCoroutine(Jump());
-        }
-        //counts the time jump key is being pressed
-        if (playerVerticalInput != 0 && !hasLongJumped)
-        {
+    IEnumerator Jump() {
+        StartCoroutine(JumpGroundCheck());
+        rb.gravityScale = 0;
+        while (Input.GetAxisRaw("Jump") != 0 && jumpKeyPressTimer < maxJumpTime) {
+            rb.velocity = new Vector2(rb.velocity.x, jumpVelocity);
             jumpKeyPressTimer += Time.deltaTime;
+            yield return null;
         }
-
-        if (playerVerticalInput == 0)
-        {
-            jumpKeyPressTimer = 0;            
+        while (jumpKeyPressTimer < minJumpTime) {
+            rb.velocity = new Vector2(rb.velocity.x, jumpVelocity);
+            jumpKeyPressTimer += Time.deltaTime;
+            yield return null;
         }
-
-        if (playerVerticalInput == 0 && grounded)
-        {
-            hasReleasedJumpDuringJump = true;
-        }
-
-        if (jumpKeyPressTimer > longJumpTriggerTime)
-        {
-            jumpTime += longJumpTimeAddition;
-            currentJumpDuration = jumpTime + longJumpTimeAddition;
-            hasLongJumped = true;
-            jumpKeyPressTimer = 0;
-        } else
-            currentJumpDuration = shortJumpTime;
-        
-
-        if (verticalDirection != 0 && !offSetting) {
-            offSetting = true;
-            verticalAccelerator = 0.01f;
-        }
-
-        var fallOffset = Mathf.Sin(Mathf.PI / 2 / Mathf.Clamp(verticalAccelerator, 0.01f, fallAccelerationDuration) / fallAccelerationDuration 
-                                   + Mathf.PI) * maxVerticalOffset;
-
-        if (jumpingUp) { 
-            jumpOffset = -Mathf.Sin(Mathf.PI / 2 / Mathf.Clamp(verticalAccelerator, 0.01f, currentJumpDuration) / currentJumpDuration 
-                                        + Mathf.PI) * maxVerticalOffset + 8;
-        }
-
-        if (verticalDirection == 1) {       
-            verticalOffset = jumpOffset;
-        }
-        else if (verticalDirection == -1) {
-            verticalOffset = fallOffset;
-        }
-
-        transform.position = GetNewPosition(transform.position);
-
-        //var x = 
-        //if (true)
-        //{
-        //    8;
-        //} else
-        //{
-        //    10;
-        //}
-
-        //x = true ? 8 : 10;
-
-        //if (jumpTimer > 0 && jumpingUp)
-        //    jumpTimer -= Time.deltaTime;
-
-        ////if (Input.GetAxisRaw("Jump") != 0) //-1 , 0 , 1
-        ////    jumpTriggerIsKeptDown = true;
-
-        //if (grounded && jumpTriggerIsKeptDown && !jumpingUp)
-        //{
-        //    if (Input.GetAxisRaw("Jump") != 0)
-        //    {
-        //        jPressTimer += Time.deltaTime;
-        //    }
-        //    if (jPressTimer > jPressTime)
-        //    {
-        //        jumpTimer = lJumpLength;
-        //        jumpingUp = true;
-        //        jumpTriggerIsKeptDown = false;
-        //    }
-        //    else if (Input.GetAxisRaw("Jump") == 0)
-        //    {
-        //        jumpTimer = sJumpUpTime;
-        //        jumpingUp = true;
-        //        jumpTriggerIsKeptDown = false;
-        //    }
-        //}
-        ////else if (jumpTimer <= 0)
-        ////    jumping = false;        
-
-        //if (jumpingUp || !grounded)
-        //{
-        //    if (jumpingUp && jumpTimer > 0)
-        //    {
-        //        jPressTimer = 0;
-        //        verticalDirection = 1;
-        //    }
-        //    else if (!grounded)
-        //    {
-        //        jumpingUp = false;
-        //        verticalDirection = -1;
-        //    }
-        //    if (jumpingUp && Input.GetAxisRaw("Jump") != 0)
-        //        return;
-        //    else if (jumpingUp && jumpTimer < 0)
-        //    {
-        //        jumpingUp = false;
-        //    }
-        //}
-        //else if (grounded)
-        //    verticalDirection = 0;
+        rb.gravityScale = defaultGravityScale;
+        jumpKeyPressTimer = 0;
     }
 
-    void FixedUpdate() { 
-        if(jumpingUp) return;
-        grounded = null != Physics2D.OverlapBox(transform.position, gcheckDimensions,
-                            0, solids);
+    IEnumerator JumpGroundCheck() {
+        float jumpWindowTimer = 0;
+        groundCheckAllowed = false;
+        while (jumpWindowTimer < jumpWindow) {
+            jumpWindowTimer += Time.deltaTime;
+            yield return null;
+        }
+        groundCheckAllowed = true;
     }
 
-    void OnDrawGizmos() {
+    void OnDrawGizmos()
+    {
         Gizmos.color = Color.red;
         Gizmos.DrawWireCube(transform.position, gcheckDimensions);
     }
