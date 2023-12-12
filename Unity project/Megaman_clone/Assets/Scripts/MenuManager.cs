679c6ec1f8a2d9757996a707e7be9f1fb2fb1659
larseemil <148450845+larseemil@users.noreply.github.com>
2023-12-12 17:59:14 +0200

    Menu updates

--- Unity project/Megaman_clone/Assets/Scripts/MenuManager.cs
+++ Unity project/Megaman_clone/Assets/Scripts/MenuManager.cs
@@ -4,40 +4,49 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
 using UnityEngine.UI;
+using Unity.VisualScripting;
 
 public class MenuManager : MonoBehaviour
 {
-    bool startSelected;
-    bool levelSelectOpen;
+    enum menuItems {start, credits, quit}
+    menuItems menuSelected;
     TMP_Text startText;
     TMP_Text quitText;
-    Image levelSelectSprite;
+    TMP_Text creditsText;
     void Start()
     {
-        startSelected = true;
+        menuSelected = menuItems.start;
         startText = GameObject.Find("StartText").GetComponent<TMP_Text>();
         quitText = GameObject.Find("QuitText").GetComponent<TMP_Text>();
+        creditsText = GameObject.Find("CreditsText").GetComponent<TMP_Text>();
     }
 
     void Update()
     {
-            if (Input.GetButtonDown("Select") && startSelected) {
-                startSelected = false;
-            } else if (Input.GetButtonDown("Select") && startSelected == false) {
-                startSelected = true;
+            if (Input.GetButtonDown("Select") && menuSelected == menuItems.start ) {
+                menuSelected = menuItems.credits;
+            } else if (Input.GetButtonDown("Select") && menuSelected == menuItems.credits) {
+                menuSelected = menuItems.quit;
+            } else if (Input.GetButtonDown("Select") && menuSelected == menuItems.quit) {
+               menuSelected = menuItems.start;
             }
-            if (startSelected == true) {
+            if (menuSelected == menuItems.start) {
                 startText.color = Color.red;
                 quitText.color = Color.white;
-            } else if (startSelected == false) {
-                quitText.color = Color.red;
+            } else if (menuSelected == menuItems.credits) {
+                creditsText.color = Color.red;
                 startText.color = Color.white;
+            } else if (menuSelected == menuItems.quit) {
+                quitText.color = Color.red;
+                creditsText.color = Color.white;
             }
-            if (Input.GetButtonDown("Start") && startSelected == true) {
+            if (Input.GetButtonDown("Start") && menuSelected == menuItems.start) {
                 SceneManager.LoadScene(1);
-            } else if (Input.GetButtonDown("Start") && startSelected == false) {
+            } else if (Input.GetButtonDown("Start") && menuSelected == menuItems.quit) {
                 Application.Quit();
                 Debug.Log("Quitting game");
+            } else if (Input.GetButtonDown("Start") && menuSelected == menuItems.credits) {
+                // Credits scene load
             }
     }
 }
