d23f7a510848927610373524217aa82cfb6b5549
larseemil <148450845+larseemil@users.noreply.github.com>
2023-12-14 12:34:17 +0200

    Tykki1.0 fixed

--- Unity project/Megaman_clone/Assets/Scripts/Tykki.cs
+++ Unity project/Megaman_clone/Assets/Scripts/Tykki.cs
@@ -3,20 +3,60 @@ using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using UnityEngine;
 
-public class ExampleScript : MonoBehaviour
+public class TykkiScript : MonoBehaviour
 {
     public GameObject tykkiprojectile;
-   
+    EnemyManager enemyManager;
+    SpriteRenderer spriteRenderer;
+    PolygonCollider2D polygonCollider;
+    Animator animator;
+    bool started;
+    Vector2Int startRoomCoords;
+    Vector2 startPos;
+    CameraMovement cam;
+    void Start() {
+        enemyManager = GetComponent<EnemyManager>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        polygonCollider = GetComponent<PolygonCollider2D>();
+        animator = GetComponent<Animator>();
+        var grid = FindObjectOfType<Grid>();
+        cam = FindObjectOfType<CameraMovement>();
+        cam.roomStart.AddListener(RoomStart);
+        cam.roomReset.AddListener(RoomReset);
+        startRoomCoords = (Vector2Int)grid.WorldToCell(transform.position);
+    }
 
-    //void Awake()
-    //{
+    void Update() {
+        if (!started) {
+            animator.enabled = false;
+            return;
+        }
+        if (enemyManager.enemyHp == 0) {
+            spriteRenderer.enabled = false;
+            polygonCollider.enabled = false;
+            animator.enabled = false;
 
-    //    InvokeRepeating("LaunchProjectile", 2.0f, 1f);
-    //}
-    
-    void LaunchProjectile()
-    {
-        
+        } else {
+            spriteRenderer.enabled = true;
+            polygonCollider.enabled = true;
+            animator.enabled = true;
+        }
+    }
+
+    void RoomReset(Vector2Int roomCoords) {
+        if (roomCoords != startRoomCoords) return;
+        print(name + " resetting");
+        enemyManager.enemyHp = enemyManager.enemyMaxHp;
+        started = false;
+    }
+
+    void RoomStart(Vector2Int roomCoords) {
+        if (roomCoords != startRoomCoords) return;
+        print(name + " starting");
+        started = true;
+    }
+
+    public void LaunchProjectile() {      
         Instantiate(tykkiprojectile, transform.position, transform.rotation);
     }
 }
