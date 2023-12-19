646bdcca574640a3fb1a0a8a156e837609380737
anabolen <148432913+anabolen@users.noreply.github.com>
2023-12-19 11:50:02 +0200

    Kurre

--- Unity project/Megaman_clone/Assets/Scripts/Buzzsaw.cs
+++ Unity project/Megaman_clone/Assets/Scripts/Buzzsaw.cs
@@ -7,20 +7,20 @@ using static UnityEngine.GraphicsBuffer;
 
 public class Buzzsaw : MonoBehaviour
 {
+    private float waitTime = 3.0f;
+    private float timer = 0.0f;
     Rigidbody2D rb;
     [SerializeField] float launchForce;
 
     // Start is called before the first frame update
     void Start()
     {
+        Destroy(gameObject, 5);
         rb = GetComponent<Rigidbody2D>();
         var x = Random.Range(-1f, 1f);
         rb.AddForce(new Vector2(x, 0) * launchForce, ForceMode2D.Impulse);
+        AudioFW.Play("BUZZSAW");
     }
 
-    // Update is called once per frame
-    //    void Update()
-    //    {
-    //        OnTriggerEnter2D()
-    //    }
+    
 }
